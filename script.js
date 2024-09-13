d051e5c2d2a2ddd46993b62d0041687c07856ec3
Jason <jasonbetsargon@gmail.com>
2024-09-12 20:31:58 -0700

    Catch division by 0 error AND prevent screen overflow

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+    if(num2 === 0){return 9999999999};
     return num1 / num2;
 }
 
@@ -18,29 +19,24 @@ function operate(num1, operator, num2) {
     switch (operator) {
         case 0:
             return add(num1, num2);
-            break;
         case 1:
             return subtract(num1, num2);
-            break;
         case 2:
             return multiply(num1, num2);
-            break;
         case 3:
             return divide(num1, num2);
-            break;
         default:
-            break;
     }
 }
 
 // Event listener function (event passed from button clicks)
 function display(e) {
+    textLength = displayVar.textContent.length;
     if(e.target.classList.contains('clear')){
         num1 = null;
         operator = null;
         num2 = null;
         displayVar.textContent = '';
-        console.log('cleared');
     } else if(e.target.classList.contains('sign')){
         displayVar.textContent = Number(displayVar.textContent) * (-1);
     } else if((e.target.classList.contains('operation')) && (num1 == null)){
@@ -55,19 +51,24 @@ function display(e) {
             operator = 3;
         }
         displayVar.textContent = e.target.innerHTML;
-        console.log('num1: ' + num1 + ' operator: ' + operator);
         clearLast = true;
     } else if(e.target.classList.contains('equals')){
         num2 = Number(displayVar.textContent);
-        console.log('num1: ' + num1 + ' operator: ' + operator + ' num2: ' + num2);
-        displayVar.textContent = Math.round(operate(num1, operator, num2)*10000)/10000;
+        if(Math.round(operate(num1, operator, num2)*10000)/10000 < 99999999){
+            displayVar.textContent = Math.round(operate(num1, operator, num2)*10000)/10000;
+        } else {
+            displayVar.textContent = "ERROR";
+        }
         num1 = null;
         operator = null;
         num2 = null; 
     } else if(e.target.classList.contains('operation') && displayVar.textContent){
         num2 = Number(displayVar.textContent);
-        console.log('num1: ' + num1 + ' operator: ' + operator + ' num2: ' + num2);
-        displayVar.textContent = Math.round(operate(num1, operator, num2)*10000)/10000;
+        if(Math.round(operate(num1, operator, num2)*10000)/10000 < 99999999){
+            displayVar.textContent = Math.round(operate(num1, operator, num2)*10000)/10000;
+        } else {
+            displayVar.textContent = "ERROR";
+        }
         num1 = Number(displayVar.textContent);
         if(e.target.classList.contains('plus')){
             operator = 0;
@@ -80,13 +81,11 @@ function display(e) {
         }
         num2 = null;
         clearLast = true;
-    } else if(e.target.classList.contains('num') && clearLast){
+    } else if(e.target.classList.contains('num') && clearLast && textLength < 8){
         displayVar.textContent = e.target.innerHTML;
-        console.log(displayVar.textContent);
         clearLast = false;
-    } else if(e.target.classList.contains('num') && !clearLast){
+    } else if(e.target.classList.contains('num') && !clearLast && textLength < 8){
         displayVar.textContent += e.target.innerHTML;
-        console.log(displayVar.textContent);
         clearLast = false;
     }
 }
@@ -98,7 +97,7 @@ let num2 = null;
 let clearLast = false;
 
 let displayVar = document.querySelector('.display')
-
+let textLength = 0;
 // Event Listener
 let input = document.querySelector('.calculator');
 
